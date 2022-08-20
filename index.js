9e8484236ec22d819de903d02054a3c65a161630
Ahmed Alli <muniirhaddadi1@gmail.com>
2022-08-20 15:48:16 +0300

    assignment complete

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Code your solutions in this file
+let namesArray =["Guadalupe", "Ollie", "Aki"]
+
+
+function writeCards( namesArray,) {
+    let cards = []
+    for ( let i = 0; i < namesArray.length; i++ ) {
+      cards.push( `Thank you, ${namesArray[i]}, for the wonderful surprise gift!` )
+    }
+    return cards
+  }
+  
+  function countDown( startingNumber ) {
+    while ( startingNumber > 0 ) {
+      console.log( startingNumber );
+      startingNumber -= 1;
+    }
+    console.log( startingNumber );
+  }
